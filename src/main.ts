6cbe41072175393143e4d4723621d53259b53bce
Joel Veloz <joevpazm@espol.edu.ec>
2025-12-04 12:30:06 -0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,12 @@ import { config } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: config.FRONTEND_CLIENT_URL, credentials: true });
+  app.enableCors({
+    origin: ['http://localhost:3000', config.FRONTEND_CLIENT_URL],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
