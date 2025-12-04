ed0858c8f96a0777f51a35669e30a4e2d3de8b1b
Joel Veloz <joevpazm@espol.edu.ec>
2025-12-04 12:35:45 -0500

    fix

--- src/howen-vss/howen-vss.module.ts
+++ src/howen-vss/howen-vss.module.ts
@@ -1,11 +1,10 @@
 import { HowenVssApiService } from './howen-vss-api.service';
 import { HowenVssAuthService } from './howen-vss-auth.service';
 import { HowenVssService } from './howen-vss.service';
-import { HowenVssWebSocketService } from './howen-vss-websocket.service';
 import { Module } from '@nestjs/common';
 
 @Module({
-  providers: [HowenVssApiService, HowenVssAuthService, HowenVssService, HowenVssWebSocketService],
-  exports: [HowenVssApiService, HowenVssAuthService, HowenVssService, HowenVssWebSocketService],
+  providers: [HowenVssApiService, HowenVssAuthService, HowenVssService],
+  exports: [HowenVssApiService, HowenVssAuthService, HowenVssService],
 })
 export class HowenVssModule {}
